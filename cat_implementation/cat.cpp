c9561c54f4f510865577a4b8bc1d967eb9301903
Sudip Shil <66914502+sudipshil9862@users.noreply.github.com>
2022-02-09 10:45:53 +0530

    changes for if 3 argument is there
    changes for if 3 argument is there like cat text1.txt cat test2.txt

--- cat_implementation/cat.cpp
+++ cat_implementation/cat.cpp
@@ -4,17 +4,37 @@
 #include<string>
 using namespace std;
 int main(int argc,char *argv[]){
-    if(argc!=2){
-        cout<<"error";
+    if(argc==1){
+        cout<<"no parameter to display or concatinate";
     }
-    ifstream myfile(argv[1]);
-    if(myfile.is_open()){
-        string line;
-        while (getline(myfile,line))
-        {
-            cout<<line<<endl;
+    if(argc == 2){
+        ifstream myfile(argv[1]);
+        if(myfile.is_open()){
+            string line;
+            while (getline(myfile,line))
+            {
+                cout<<line<<endl;
+            }
+            myfile.close();
         }
-        myfile.close();
+    }
+    if(argc == 3){
+        ifstream myfile1(argv[1]);
+        ifstream myfile2(argv[2]);
+        if(myfile1.is_open() && myfile2.is_open()){
+            string line1,line2;
+            while (getline(myfile1,line1))
+            {
+                cout<<line1<<endl;
+            }
+            while (getline(myfile2,line2))
+            {
+                cout<<line2<<endl;
+            }
+            myfile1.close();
+            myfile2.close();
+            
+        }
     }
     
 
